c9401aae59d9c4e9435f85af565b5d70fc3007fd
lino994 <linosperez@gmail.com>
2016-11-26 18:08:59 -0600

    Login Works now.. Connects to Server using PHP File in LoginASync Class

--- app/src/main/java/com/example/linos/testapp/MainActivity.java
+++ app/src/main/java/com/example/linos/testapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.linos.testapp;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,38 +15,180 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String LINK = "http://galadriel.cs.utsa.edu/~group5/testConnect.php";
     Button btn;
     EditText medit;
     EditText epass;
     String uname;
     String pass;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        medit   = (EditText)findViewById(R.id.edUsername);
-        epass = (EditText)findViewById(R.id.edPassword);
+        medit = (EditText) findViewById(R.id.edUsername);
+        epass = (EditText) findViewById(R.id.edPassword);
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-    public void Login(View view) {
 
-        uname = medit.getText().toString();
-        pass = epass.getText().toString();
-        Log.v("Username", uname);
-        Log.v("Password", pass);
-        if(pass.equals("admin") && uname.equals("admin")){
-            Intent myIntent = new Intent(view.getContext(), Messaging.class);
-            startActivityForResult(myIntent, 0);
+    public void Login(View view) {
+        String uname=medit.getText().toString();
+        String pass=epass.getText().toString();
+
+        callLogin(uname, pass, view);
+    }
+
+    public void callLogin(String uname, String pass, final View view){
+        class LoginASync extends AsyncTask <String, Void, String>{
+            //uname=medit.getText().toString();
+            //String pass=epass.getText().toString();
+
+            //Log.v("Username",uname);
+            //Log.v("Password",pass);
+            private Dialog loadingDiag;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loadingDiag = ProgressDialog.show(MainActivity.this,"Please Wait","loading..");
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                String uri = params[0];
+                String uname = params[1];
+                String pass = params[2];
+
+                try{
+                    /******************TEST USERS****************************
+                        Username : UserOne
+                        Password : p@ss1
+
+                        Username : UserTwo
+                        Password : p@ss2
+
+                     ****************************************************/
+                    String data = URLEncoder.encode("uname", "UTF-8")
+                            + "=" + URLEncoder.encode(uname, "UTF-8");
+                    data += "&" + URLEncoder.encode("pass", "UTF-8")
+                            + "=" + URLEncoder.encode(pass, "UTF-8");
+                    URL url = new URL(uri);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                    connection.setDoOutput(true);
+                    OutputStreamWriter osWrite = new OutputStreamWriter(connection.getOutputStream());
+                    osWrite.write(data);
+                    osWrite.flush();
+
+                    BufferedReader bReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+                    while((line = bReader.readLine()) != null) {
+                        Log.v("line: ", line);
+                        sb.append(line + "\n");
+                    }
+                        Log.v("string :", sb.toString());
+                        return sb.toString();
+
+                }catch(Exception e){
+                    Log.v("Conn Error  :", e.getMessage());
+                    return new String ("Exception: " + e.getMessage());
+                }
+            }
+            @Override
+            protected void onPostExecute(String result) {
+                loadingDiag.dismiss();
+                String r = result.trim();
+                Log.v("result: ", r);
+                if(r.equals("success")){
+                    Intent myIntent = new Intent(view.getContext(), Messaging.class);
+                    startActivityForResult(myIntent, 0);
+                }else if (r.equals("failure")) {
+                    Toast.makeText(getApplicationContext(), "incorrect username/password", Toast.LENGTH_SHORT).show();
+                }else {
+                    Log.v("Result Error", result);
+                }
+            }
+
+           /* if(pass.equals("admin")&&uname.equals("admin"))
+
+            {
+                Intent myIntent = new Intent(view.getContext(), Messaging.class);
+                stuserartActivityForResult(myIntent, 0);
+            }
+
+            else
+
+            {
+                Toast.makeText(getApplicationContext(), "incorrect username/password", Toast.LENGTH_SHORT).show();
+            }*/
+
         }
-        else {
-            Toast.makeText(getApplicationContext(),"incorrect username/password",Toast.LENGTH_SHORT).show();
-        }
+
+        LoginASync las = new LoginASync();
+        las.execute(LINK ,uname, pass);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
